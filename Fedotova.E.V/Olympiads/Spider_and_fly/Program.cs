06b63157f6cb04fd4b9b1dd26cbcf535ad7defe4
Валерий <valera.salaanov@mail.ru>
2023-10-21 10:24:04 +0600

    Olympiads

---
+++ Fedotova.E.V/Olympiads/Spider_and_fly/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Чтение входных данных
+        Console.WriteLine("Введите ширину, глубину и высоту комнаты (через пробел):");
+        string[] dimensions = Console.ReadLine().Split(' ');
+        int a = int.Parse(dimensions[0]);
+        int b = int.Parse(dimensions[1]);
+        int c = int.Parse(dimensions[2]);
+
+        Console.WriteLine("Введите координаты паука (Sx, Sy, Sz) относительно начала комнаты (0, 0) (через пробел):");
+        string[] spiderCoords = Console.ReadLine().Split(' ');
+        int Sx = int.Parse(spiderCoords[0]);
+        int Sy = int.Parse(spiderCoords[1]);
+        int Sz = int.Parse(spiderCoords[2]);
+
+        Console.WriteLine("Введите координаты мухи (Fx, Fy, Fz) относительно начала комнаты (0, 0) (через пробел):");
+        string[] flyCoords = Console.ReadLine().Split(' ');
+        int Fx = int.Parse(flyCoords[0]);
+        int Fy = int.Parse(flyCoords[1]);
+        int Fz = int.Parse(flyCoords[2]);
+
+        // Вычисление минимального расстояния
+        double distance = Math.Sqrt(Math.Pow(Sx - Fx, 2) + Math.Pow(Sy - Fy, 2) + Math.Pow(Sz - Fz, 2));
+
+        // Вывод результата
+        Console.WriteLine("Минимальное расстояние, которое необходимо проползти пауку, чтобы добраться до мухи: {0:F3}", distance);
+    }
+}
---
+++ Fedotova.E.V/Olympiads/The_peasant_and_the_devil/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите начальное количество монет (N):");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество монет, отдаваемых черту (K):");
+        int K = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество переходов через мост (Z):");
+        int Z = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        if (CalculateMoney(N, K, Z) == 0)
+        {
+            count++;
+        }
+
+        Console.WriteLine("Количество комбинаций условий перехода через мост: " + count);
+    }
+
+    static int CalculateMoney(int N, int K, int Z)
+    {
+        int money = N;
+
+        for (int i = 1; i <= Z; i++)
+        {
+            money *= 2;
+            money -= K;
+
+            if (money < 0)
+            {
+                return money;
+            }
+        }
+
+        return money;
+    }
+}
